da97c2c729df4fd3d77613ad8a02e0ead3b07e6c
xach55 <xach55@gmail.com>
2024-04-29 21:19:07 +0400

    initial commit

---
+++ SwiftUITeplate/SwiftUITeplate/SwiftUITeplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITeplateApp.swift
+//  SwiftUITeplate
+//
+//  Created by Levon Shaxbazyan on 29.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITeplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
